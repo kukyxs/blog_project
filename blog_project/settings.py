83c06a7bd85108b24b3de97c77130007220a0cc8
kuky <kukyxingshi@163.com>
2018-04-10 00:03:09 +0800

    增加分页和多条件筛选

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -75,7 +75,7 @@ REST_FRAMEWORK = {
     # 配置全局分页类型和每页数量
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
 }
 
 WSGI_APPLICATION = 'blog_project.wsgi.application'
